015bf31abfd83fea3eaff7fc371efac85d099ad2
FiveRage <vladislav.shishkin.1998@yandex.ru>
2018-11-26 11:55:46 +0200

    some init msg

--- speechdo.py
+++ speechdo.py
@@ -3,17 +3,14 @@ import usb.core
 import usb.util
 import time
 
-print("test")
 dev = usb.core.find(idVendor=0x2886, idProduct=0x0018)
 #print dev
 if dev:
     Mic_tuning = Tuning(dev)
-    print("enable")
     while True:
         try:
             if Mic_tuning.is_voice():
                 print Mic_tuning.direction
                 time.sleep(1)
         except KeyboardInterrupt:
-            print "error"
             break
